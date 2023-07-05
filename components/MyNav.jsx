a6c080e19c42454ab71c31d9ea192facdd95bcaa
mohammed-taherali <mohammedtaherali@gmail.com>
2023-07-05 21:08:19 +0530

    working navbar

--- components/MyNav.jsx
+++ components/MyNav.jsx
@@ -6,28 +6,28 @@ import React from 'react';
 
 export default function MyNav() {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary custom-bg">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary px-5 fixed-top custom-bg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#"><img src="./mtc-logo.png" className="logo-img" /> <span id='myName'>Mohammed</span></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+                    <ul class="navbar-nav push-right">
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><img src='./mtc-logo-cropped.png' /></a>
+                            <a class="nav-link" aria-current="page" href="#">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" href="#">Skills</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" href="#">Projects</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
+                            <a class="nav-link" href="#">Education</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
+                            <a class="nav-link" href="#">Contact</a>
                         </li>
                     </ul>
                 </div>
@@ -35,3 +35,41 @@ export default function MyNav() {
         </nav>
     )
 }
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import React from 'react';
+
+// export default function MyNav() {
+//     return (
+//         <nav class="navbar navbar-expand-lg bg-body-tertiary custom-bg">
+//             <div class="container-fluid">
+//                 <a class="navbar-brand" href="#">Mohammed</a>
+//                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//                     <span class="navbar-toggler-icon"></span>
+//                 </button>
+//                 <div class="collapse navbar-collapse" id="navbarNav">
+//                     <ul class="navbar-nav">
+//                         <li class="nav-item push-right">
+//                             <a class="nav-link" href="#"><img src="./Untitled.png" className="logo-img" /></a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link active" aria-current="page" href="#">Home</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Features</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Pricing</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link disabled">Disabled</a>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </div>
+//         </nav>
+//     )
+// }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
